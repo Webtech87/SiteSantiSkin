e40d0f8c6cafc7d17705c359ae5105dda8680bb1
boris <boris.isac@webtech87.com>
2025-05-09 12:59:51 +0100

    [ADD] serializer for users

--- server/users/urls.py
+++ server/users/urls.py
@@ -4,9 +4,22 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 from django.urls import path
+from .views import CustomViewSet
+
+name_app = 'users'
 
 urlpatterns = [
-
+    path('',
+         CustomViewSet.as_view(
+            {
+                'get': 'list',
+                 'post': 'create',
+                 'put': 'update',
+                 'delete': 'destroy'
+            }
+        ),
+         name='users'
+    ),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
--- server/users/views.py
+++ server/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
+
+from .models import CustomUser
+from .serializers import UserSerializer
+
 
 # Create your views here.
+class CustomViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == 'list':
+            return [IsAdminUser()]
+        if self.action == 'create':
+            return [AllowAny()]
+        return [IsAuthenticated()]
