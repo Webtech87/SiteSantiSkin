5e0c576d26beb893ea33b41152f6bdb527485aae
BIWebtech87 <boris.isac@webtech87.pt>
2025-05-15 11:14:53 +0100

    [FIX] Dr create new obj. dr_lisence

--- server/podcast/models.py
+++ server/podcast/models.py
@@ -1,5 +1,3 @@
-from email.policy import default
-
 from django.db import models
 from drs.models import Dr
 from django.utils import timezone
@@ -8,7 +6,7 @@ class Podcast(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     link = models.URLField()
-    date = models.DateField(default=timezone.now())
+    date = models.DateField(timezone.now().date())
     leading_dr = models.ForeignKey(Dr, on_delete=models.CASCADE, related_name='podcasts')
 
     def __str__(self):
--- server/server/settings.py
+++ server/server/settings.py
@@ -15,15 +15,18 @@ import os
 from dotenv import load_dotenv
 from datetime import timedelta
 
-load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
